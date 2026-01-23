7e18f7ed9e0acb3bbf490b332943cbc9b8498214
mansisgit <mansicc15@gmail.com>
2026-01-23 15:21:58 +0530

    git

--- src/Demo.java
+++ src/Demo.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(10);
         list.add(20);
-        list.remove(1);
-        System.out.println(list.remove(Integer.valueOf(10)));
+        //list.remove(1);
+        System.out.println(list.remove(1)+"removed by index");
+        System.out.println(list.remove(Integer.valueOf(10))+"by value/object");
+        System.out.println(Arrays.toString(list.toArray()));
 
     }
 
