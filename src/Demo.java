a5b95d8dd322fe70fffea122611d3c47ad9e52ee
mansisgit <mansicc15@gmail.com>
2026-01-23 15:18:47 +0530

    git

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Demo {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.remove(1);
+        System.out.println(list.remove(Integer.valueOf(10)));
 
     }
 
