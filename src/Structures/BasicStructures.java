ca2bb6c2316164cddf53e99df435b29d59a1fe24
mansisgit <mansicc15@gmail.com>
2026-01-24 12:00:03 +0530

    git

---
+++ src/Structures/BasicStructures.java
@@ -0,0 +1,20 @@
+package Structures;
+
+public class BasicStructures {
+    public static void main(String[] args) {
+        //knowing these at first imp---->
+
+        //array    //hashmap
+        //HashSet  //stack
+
+        /*
+        1.create memory
+        2.iterate
+        3.check condition-----Y
+        4.update memory       |
+        5. return result <----|
+        * */
+
+
+    }
+}
