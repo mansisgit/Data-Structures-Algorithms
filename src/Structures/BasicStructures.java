b6c963af63c30361f95c866c7117d5f3c093ce91
mansisgit <mansicc15@gmail.com>
2026-01-25 19:05:38 +0530

    git

--- src/Structures/BasicStructures.java
+++ src/Structures/BasicStructures.java
@@ -1,5 +1,9 @@
 package Structures;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class BasicStructures {
     public static void main(String[] args) {
         //knowing these at first imp---->
@@ -15,6 +19,19 @@ public class BasicStructures {
         5. return result <----|
         * */
 
+        System.out.println(code("cattcc"));
 
+    }
+    public static String code(String word){
+        //e1a1t1
+        TreeMap<Character,Integer> map = new TreeMap<>();
+        for(char c : word.toCharArray()) {
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        StringBuffer res = new StringBuffer();
+        for(Map.Entry<Character,Integer> ent : map.entrySet()){
+            res.append(ent.getKey()).append(ent.getValue());
+        }
+        return res.toString();
     }
 }
