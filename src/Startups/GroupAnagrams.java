40a0824ba01a76f913d3206c9f11690fbe07cd2f
mansisgit <mansicc15@gmail.com>
2026-01-26 22:22:17 +0530

    git

--- src/Startups/GroupAnagrams.java
+++ src/Startups/GroupAnagrams.java
@@ -2,6 +2,21 @@ package Startups;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
-
+        System.out.println(code("Mannnsi"));
     }
+
+    //creating unique code for each word to compare
+    public static String code(String word){
+        StringBuffer br = new StringBuffer();
+        int[] freq = new int[26];
+        for(char c : word.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(int ele : freq){
+            if(ele>0){
+
+            }
+        }
+    }
+
 }
