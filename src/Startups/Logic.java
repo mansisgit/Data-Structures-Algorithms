886f91014cccbabd8c4708ef7b1e84029acd0559
mansisgit <mansicc15@gmail.com>
2026-01-28 19:47:36 +0530

    git

--- src/Startups/Logic.java
+++ src/Startups/Logic.java
@@ -1,4 +1,28 @@
 package Startups;
 
 public class Logic {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        /*
+        * class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //prepare memory
+      HashMap<Integer,Integer> map = new HashMap<>();
+     //iterate for? --> each
+      for(int i=0;i<nums.length;i++){
+        //condition to get ans for the first ele checking
+        int comp = target - nums[i];
+        if(map.containsKey(comp)) return new int[] {map.get(comp),i};//immidiate return is also possible
+        //then operation for next elements
+        map.put(nums[i],i);
+      }
+      //returning stored ans after condition satisfaction
+    return new int[] {-1,-1};
+    }
+}
+        * */
+
+    }
 }
