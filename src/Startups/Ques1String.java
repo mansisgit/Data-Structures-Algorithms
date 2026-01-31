8ecae0c0ffc49b2d1110db96b93e000c16084b63
mansisgit <mansicc15@gmail.com>
2026-01-31 14:13:50 +0530

    git

--- src/Startups/Ques1String.java
+++ src/Startups/Ques1String.java
@@ -1,4 +1,23 @@
 package Startups;
 
+import java.util.HashMap;
+
 public class Ques1String {
+    public static void main(String[] args) {
+        String sample = "absdiu kusdgfiuef gjfbgur njkdhfed";
+        System.out.println(validSubStrings(sample));
+    }
+
+   public static int validSubStrings(String sam){
+        int count =0;
+        sam=sam.trim();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
+        freq[0]=1;
+        freq[1]=1;
+        for(int i=2;i<26;i+=3){
+            freq[i]
+        }
+   }
+
 }
