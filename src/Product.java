24c361f8be813ffca176c2d599673c3f11838641
mansisgit <mansicc15@gmail.com>
2026-02-02 11:05:54 +0530

    git

---
+++ src/Product.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+    public Product(String name, String price){
+        this.name= name;
+        this.price = new BigDecimal(price);
+
+    }
+
+    public BigDecimal getRoundedPrice(int decimalPlaces){
+        return price.setScale(decimalPlaces, RoundingMode.UP);
+
+    }
+    public static void main(String[] args) {
+        Product item = new Product("wedge","10.345");
+        BigDecimal rounded = item.getRoundedPrice(2);
+        System.out.println("original price:"+item.price);
+        System.out.println("rounded price:" + rounded);
+    }
+
+
+}
